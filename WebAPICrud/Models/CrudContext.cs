267d532a041ea2a8696d3af935e75b7d172265f1
Yahel34 <Yahel34@DESKTOP-II6QOOU>
2024-09-16 10:19:22 -0600

    Agregar archivos de proyecto.

---
+++ WebAPICrud/Models/CrudContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace WebAPICrud.Models;
+
+public partial class CrudContext : DbContext
+{
+    public CrudContext()
+    {
+    }
+
+    public CrudContext(DbContextOptions<CrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.IdEmpleado).HasName("PRIMARY");
+
+            entity.ToTable("empleado");
+
+            entity.HasIndex(e => e.IdEmpleado, "idEmpleado_UNIQUE").IsUnique();
+
+            entity.Property(e => e.IdEmpleado)
+                .ValueGeneratedNever()
+                .HasColumnName("idEmpleado");
+            entity.Property(e => e.Correo).HasMaxLength(80);
+            entity.Property(e => e.Nombre).HasMaxLength(60);
+            entity.Property(e => e.Sueldo).HasColumnName("sueldo");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ WebAPICrud/Program.cs
@@ -0,0 +1,35 @@
+using WebAPICrud.Models;
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<CrudContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    var serverVersion = ServerVersion.AutoDetect(connectionString); // Detecta la versión del servidor automáticamente
+    options.UseMySql(connectionString, serverVersion);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
